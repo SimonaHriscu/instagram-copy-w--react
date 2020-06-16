798b6a9c154a1a017379b205d6613f0d294ce754
Simona <see.moona@yahoo.com>
2020-06-16 17:18:31 +0200

    Updates

---
+++ precache-manifest.5c50fcca54b1057eee7bc6d47a982742.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba1097634a3eec2c09843918415dd325",
+    "url": "/Instagram-copy-w--React/index.html"
+  },
+  {
+    "revision": "be08e6307f9b5f1adea9",
+    "url": "/Instagram-copy-w--React/static/css/main.362b42db.chunk.css"
+  },
+  {
+    "revision": "339a5072926f45eda2c8",
+    "url": "/Instagram-copy-w--React/static/js/2.0c2c3197.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Instagram-copy-w--React/static/js/2.0c2c3197.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be08e6307f9b5f1adea9",
+    "url": "/Instagram-copy-w--React/static/js/main.9da54893.chunk.js"
+  },
+  {
+    "revision": "36ebf017e6b9988327cd",
+    "url": "/Instagram-copy-w--React/static/js/runtime-main.43ff17b0.js"
+  },
+  {
+    "revision": "dcc389e54c4374fb8ccf2e8fc2ecedf9",
+    "url": "/Instagram-copy-w--React/static/media/logo.dcc389e5.png"
+  },
+  {
+    "revision": "d1a4e7dd48a249c6d201b0a74f5f6c18",
+    "url": "/Instagram-copy-w--React/static/media/profile-pic.d1a4e7dd.png"
+  },
+  {
+    "revision": "46ec9691d4a7eedd26bd0d05608c6349",
+    "url": "/Instagram-copy-w--React/static/media/stories.46ec9691.png"
+  }
+]);
